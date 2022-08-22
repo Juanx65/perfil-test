1f586b20373d0ab8e4532f04113736f723860b29
juanx65 <juan.aguileraca@sansano.usm.cl>
2022-08-21 22:34:06 -0400

    cleaner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
+
+import { personOutline, personSharp } from 'ionicons/icons';
 
 import Header from '../components/Header/Header';
 import GrayBox from '../components/Modals/GrayBox';
@@ -12,7 +14,19 @@ const Home: React.FC = () => {
     <IonPage>
       <Header title={usr!.toString()} img_usr={img_usr!.toString()} dash={true} />
       <IonContent fullscreen>
-        <GrayBox/>
+        <IonGrid>
+          <IonRow className='homeRow'>
+            <IonItem lines='none' className='titleHome'>Cargo:</IonItem>
+            <IonItem lines='none' className='contentHome'>Contador</IonItem>
+          </IonRow>
+          <IonRow className='homeRow'>
+            <IonItem lines='none' className='titleHome'>Periodo Actual:</IonItem>
+            <IonItem lines='none' className='contentHome'>Agosto 2022</IonItem>
+          </IonRow>
+          <IonItem><strong>Información Personal</strong><IonIcon slot='end' md={personSharp} ios={personSharp}/></IonItem>
+        </IonGrid>
+
+        <GrayBox />
       </IonContent>
     </IonPage>
   );
